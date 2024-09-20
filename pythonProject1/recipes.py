8db653fe745cc0daabdcc70ffeb99597e9c1ba0d
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-09-20 16:59:47 +0100

    Called recipe api and got response

--- pythonProject1/recipes.py
+++ pythonProject1/recipes.py
@@ -1,1 +1,10 @@
 import requests
+
+def recipe_search(ingredient):
+    app_id = '35b30cea'
+    app_key = '2422db83b20100056b7fcc54c5915c02'
+    result = requests.get(f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}')
+    data = result.json()
+    return data['hits']
+
+recipe_search('cheese')
